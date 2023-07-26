edcc130a4e4403d6d93dfc2e7d4434fedc64dd1e
KaiZerg <gabitov.tatarin@gmail.com>
2023-07-26 19:41:56 +0500

    Optimized code

--- src/test/java/selenide/DragAndDropTests.java
+++ src/test/java/selenide/DragAndDropTests.java
@@ -21,13 +21,10 @@ public class DragAndDropTests extends TestBase {
     @Test
     void dragAndDropToTest () {
         open("https://the-internet.herokuapp.com/drag_and_drop");
-        // Check column-a header before dragAndDropTo
         $("#column-a header").shouldHave(text("A"));
-        // Check column-b header before dragAndDropTo
         $("#column-b header").shouldHave(text("B"));
         columnA.dragAndDropTo(columnB);
         $("#column-a header").shouldHave(text("B"));
-        // Check column-b header after dragAndDropTo
         $("#column-b header").shouldHave(text("A"));
     }
 }
